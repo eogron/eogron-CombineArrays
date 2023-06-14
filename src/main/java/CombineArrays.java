0228cb41790804d0fc8cdac6207eee517dd97b45
Ezekiel Ogron <ezekielogron@gmail.com>
2023-06-14 00:24:06 +0000

    completed

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,13 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] newArray = new int[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            newArray[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            newArray[arr1.length + i] = arr2[i];
+        }
+        return newArray;
     }
 }
